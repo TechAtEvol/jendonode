6b6f1cc2efe658a19c68b93b209a228ece6d3671
Staffan Frisk <staffan@evol.se>
2017-12-29 00:20:18 +0100

    scaffolded example site in place

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jendonode
 Jenkins, Docker, Node JS. Infrastructure as code. An exercise.
+
+# Install
+npm install
+
+# Start
+npm start
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'A simple site managed by Jenkins and docker' });
+});
+
+module.exports = router;
